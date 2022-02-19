443980ec540c15ec8710fef48452659f594477d0
Alex <a.pototskiy@codefresh.io>
2022-02-19 19:20:22 +0100

    Add util formatters

---
+++ libs/store/util-formatters/src/lib/store-util-formatters.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export { formatRating } from './store-util-formatters';
+
+@NgModule({
+  imports: [CommonModule],
+})
+export class StoreUtilFormattersModule {}
