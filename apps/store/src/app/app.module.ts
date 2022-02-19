739409dc1b2a148e2fef7346a59fbf9cf574fba9
Alex <a.pototskiy@codefresh.io>
2022-02-19 19:03:29 +0100

    Add lib dependency

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     MatCardModule,
     BrowserModule,
+    StoreUiSharedModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
