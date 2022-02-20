8fbaa7d95d05968fb9656160183e230572fa78d9
Alex <a.pototskiy@codefresh.io>
2022-02-20 11:45:25 +0100

    Displaying a full game

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
@@ -10,6 +10,7 @@ import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
+    HttpClientModule,
     MatCardModule,
     BrowserModule,
     StoreUiSharedModule,
@@ -19,7 +20,7 @@ import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
           path: 'game/:id',
           loadChildren: () =>
             import('@bg-hoard/store/feature-game-detai').then(
-              (module) => module.StoreFeatureGameDetaiModule
+              (module) => module.StoreFeatureGameDetailModule
             ),
         },
       ],
